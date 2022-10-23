8be6121e27b1e0fa7fd83a7f95a24ff7c73bf80b
Bakenneko <andronniy7@gmail.com>
2022-10-24 01:41:05 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
+import {Moviebox} from "./service/Moviebox";
+import {API_URL} from "./service/urls";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res)=>res.json())
+            .then(data=>{
+                console.log(data);
+                setMovies(data.results);
+            })
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="grid">
+            {movies.map((movieReq) => <Moviebox key={movieReq.id} {...movieReq}/>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
